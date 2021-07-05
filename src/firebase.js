1148dcb008d51da5480b40c562b83895b1904db1
shanfar basha <shanfar786@gmail.com>
2021-07-05 19:35:09 +0530

    new changr232

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import "firebase/auth"
 import firebase from 'firebase/app';
 // import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({
+const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -13,5 +13,5 @@ const firebaseApp = firebase.initializeApp({
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
-export const auth = firebaseApp.auth()
-export default firebaseApp
+export const auth = app.auth()
+export default app
